a7124d6d219db50c4b7b8f4e2f30384052c13c98
Samyuk Yadav Banda <aronisam325@gmail.com>
2021-12-01 21:10:20 +0000

    Made Register and Login for App

---
+++ app/src/main/java/com/example/newfirebase/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.newfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
+    private EditText edit;
+    private Button add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this , StartActivity.class));
+            }
+        });
+    }
+}
