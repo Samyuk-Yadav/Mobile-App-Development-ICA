7b630ed36e44457d91f6fcec7bc6622f63743235
Samyuk Yadav Banda <aronisam325@gmail.com>
2021-12-02 03:26:17 +0000

    Updateing and Retreiveing Data from Firebase

--- app/src/main/java/com/example/newfirebase/MainActivity.java
+++ app/src/main/java/com/example/newfirebase/MainActivity.java
@@ -1,25 +1,41 @@
 package com.example.newfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
     private EditText edit;
     private Button add;
+
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +46,54 @@ public class MainActivity extends AppCompatActivity {
         edit = findViewById(R.id.edit);
         add = findViewById(R.id.add);
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        listview = findViewById(R.id.listview);
+
+        logout.setOnClickListener((v) -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, StartActivity.class));
+        });
+
+        add.setOnClickListener(v -> {
+            String txt_name = edit.getText().toString();
+            if (txt_name.isEmpty()) {
+                Toast.makeText(MainActivity.this, "No Name Entered", Toast.LENGTH_SHORT).show();
+            } else {
+                FirebaseDatabase.getInstance().getReference().child("ProgramLogDemo").child("AndroidAppDevelopment").push().setValue(txt_name);
+            }
+        });
+
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list);
+        listview.setAdapter(adapter);
+
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Information");
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this , StartActivity.class));
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Information info = snapshot.getValue(Information.class);
+                    String txt = info.getName() + " : " + info.getEmail();
+                    list.add(txt);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        DocumentReference ref = FirebaseFirestore.getInstance().collection("Cities").document("txt");
+        ref.update("capital" , "Belfast").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Sucessfully updated", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
