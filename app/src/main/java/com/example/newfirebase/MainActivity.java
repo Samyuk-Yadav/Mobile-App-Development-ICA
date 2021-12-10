9bc35b65d4bbc32f86ea792dec7ad36aefbcb484
Samyuk Yadav Banda <aronisam325@gmail.com>
2021-12-10 00:17:02 +0000

    creating multiple games1 compleated and night mode activated.

--- app/src/main/java/com/example/newfirebase/MainActivity.java
+++ app/src/main/java/com/example/newfirebase/MainActivity.java
@@ -1,20 +1,75 @@
 package com.example.newfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private Button logout;
     private Button games;
+    private SwitchCompat switchCompat;
+    SharedPreferences sharedPreferences = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        games= findViewById(R.id.button1);
+
+        games= findViewById(R.id.btngames);
+
+        switchCompat = findViewById(R.id.switchCompact);
+
+
+        sharedPreferences = getSharedPreferences("Night" , 0);
+        Boolean booleanValue = sharedPreferences.getBoolean("Night_Mode", true);
+
+        if(booleanValue){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchCompat.setChecked(true);
+        }
+
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchCompat.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("Night_Mode", true);
+                    editor.commit();
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchCompat.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("Night_Mode", false);
+                    editor.commit();
+                }
+            }
+        });
+
+
+
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener((v) -> {
+            FirebaseAuth.getInstance().signOut();
+            Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, Start_Activity.class));
+        });
+
+
         games.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
